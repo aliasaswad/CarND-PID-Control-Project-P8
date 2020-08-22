2752b9ef623e212ffc9edf4cd8649b398f2c5954
aliasaswad <alias_aswad@yahoo.com>
2020-08-22 15:43:19 -0500

    try prop PID

--- scr/main.cpp
+++ scr/main.cpp
@@ -37,7 +37,9 @@ int main() {
   /**
    * TODO: Initialize the pid variable.
    */
-
+  // Proportion PID
+  pid.Init(1, 0.0, 0.0);
+  
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
